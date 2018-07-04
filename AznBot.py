1d5a2478ed02de63845b068bf55af9307994e6a3
VallyMan <40832721+VallyMan@users.noreply.github.com>
2018-07-04 20:50:59 +0200

    Added Help command

--- AznBot.py
+++ AznBot.py
@@ -11,8 +11,24 @@ client = discord.Client()#Creates Client
 bot = commands.Bot(command_prefix='!')#Sets prefix for commands(!Command)
 
 #Code
+#Resource Crawling
 @bot.command()
-async def SexCommand():
+async def wWikipedia(msg):
+    pagetoget = ('http://simple.wikipedia.org/wiki/'+msg)
+    page = requests.get(pagetoget)
+    soup = BeautifulSoup(page.content, "lxml")
+    allTexts = soup.find_all('b')
+    for i in range(0, 8):
+        printstr = (i+1, " : ", allTexts[i])
+        await bot.say(printstr)
+
+#Memes
+@bot.command()
+async def mP():
+    await bot.say(':point_right:')
+
+@bot.command()
+async def mSex():
     await bot.say('S')
     asyncio.sleep(1)
     await bot.say('E')
@@ -22,17 +38,9 @@ async def SexCommand():
     await bot.say('SEX')
     asyncio.sleep(1)
 
+#Random Stuff
 @bot.command()
-async def Wikipedia():
-    page = requests.get('http://simple.wikipedia.org')
-    soup = BeautifulSoup(page.content, "lxml")
-    allTexts = soup.find_all('b')
-    for i in range(0, 8):
-        printstr = (i+1, " : ", allTexts[i])
-        await bot.say(printstr)
-        
-@bot.command()
-async def CoinFlip():
+async def rCoin():
     Result = ""
     RandomNumber = random.randint(0,1)
     if(RandomNumber == 0):
@@ -41,15 +49,23 @@ async def CoinFlip():
         Result = "Tails"
     await bot.say(Result)
     
+
 @bot.command()
-async def Message(msg):
-    await bot.say(msg)
-    
-@bot.command()
-async def RNG(MinNumber: int , MaxNumber: int):
+async def rRNG(MinNumber: int , MaxNumber: int):
     RandomNumber = random.randint(MinNumber,MaxNumber)
     await bot.say(str(RandomNumber))
-    
+
+#Help
+@bot.command()
+async def Help():
+    #Title
+    embed = discord.Embed(title="Help")
+    #Help
+    embed.add_field(name="Meme Commands",value="mP-->Points the classical pointy finger at your target\nmSex-->Spells out the classical SEX meme")
+    embed.add_field(name="Random/Test Commands",value="rCoin-->Flips a coin, heads or tails, buddy.\nrRng(MinNumber, MaxNumber)-->Generates a random number between the provided integers")
+    embed.add_field(name="Webcrawling Commands",value="wWikipedia(ArticleName)-->Takes data from the given article")
+    await bot.say(embed=embed)
+
 #Code to connect py with bot
 bot.run('NDYzOTI2NTU5OTE3NjA0ODY0.Dh3sng.0awcnUd5UQbQ201arZVkllA980c')
     
