b91cae3be1c4129080c6d1c6566a590a1cbaf3e8
Glen Wallace <34867563+Dougvacs@users.noreply.github.com>
2018-07-04 20:30:39 +0200

    Messing with function "overloading"

--- GlenScript.py
+++ GlenScript.py
@@ -4,11 +4,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-def aznCrawl():
-    page = requests.get('http://simple.wikipedia.org')
-    soup = BeautifulSoup(page.content, "lxml")
-    allTexts = soup.find_all('b')
-    for i in range(0, 8):
-        print(i+1, " : ", allTexts[i])
+def aznCrawl(toprint = None):
+    if toprint == None:
+        page = requests.get('http://simple.wikipedia.org')
+        soup = BeautifulSoup(page.content, "lxml")
+        allTexts = soup.find_all('b')
+        for i in range(0, 8):
+            print(i+1, " : ", allTexts[i])
+    else:
+        print(toprint)
 
-aznCrawl()
+
+aznCrawl("YE")
