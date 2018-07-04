4f4fb5468a5ac7cad682d7b7d286544794526766
Glen Wallace <34867563+Dougvacs@users.noreply.github.com>
2018-07-04 17:47:43 +0200

    Changed something small

--- GlenScript.py
+++ GlenScript.py
@@ -1,10 +1,10 @@
 # Notes go up here
 # I like structuring python scripts like C
 
-import random
+import random as rnd
 
 def main():
-    randomNumber = random.randint(1, 10)
+    randomNumber = rnd.randint(1, 10)
     print(randomNumber)
     
 main()
