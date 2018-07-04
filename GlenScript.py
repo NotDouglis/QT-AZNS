96a8a6bbd3c42052b86c07be6ea218d452e9a97e
Glen Wallace <34867563+Dougvacs@users.noreply.github.com>
2018-07-04 18:06:48 +0200

    Testing out downloading the content of wikipedia

--- GlenScript.py
+++ GlenScript.py
@@ -1,15 +1,10 @@
 # Notes go up here
 # I like structuring python scripts like C
 
-<<<<<<< HEAD
-import random as rnd
+import requests
 
-=======
-import random
-#u suck hahahaha
->>>>>>> e5d116e6e2279e20d58a6b0d53a3b4225070ac4d
 def main():
-    randomNumber = rnd.randint(1, 10)
-    print(randomNumber)
+    page = requests.get('https://simple.wikipedia.org/wiki/Main_Page')
+    print(page.content)
     
 main()
