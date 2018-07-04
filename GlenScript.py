d051bfab09f4e11aad3c16cf2cfe7a3aa0de1fdc
Glen Wallace <34867563+Dougvacs@users.noreply.github.com>
2018-07-04 17:39:45 +0200

    Made a random number thing as a test

--- GlenScript.py
+++ GlenScript.py
@@ -1,9 +1,10 @@
 # Notes go up here
 # I like structuring python scripts like C
 
+import random
+
 def main():
     randomNumber = random.randint(1, 10)
     print(randomNumber)
-    
     
 main()
