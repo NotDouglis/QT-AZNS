677bd7265ef53627f265fc7ef09924d6d24c6c58
VallyMan <40832721+VallyMan@users.noreply.github.com>
2018-07-18 09:13:12 +0200

    Base Array

--- ChenRogueLite.py
+++ ChenRogueLite.py
@@ -1,1 +1,14 @@
-print("Hey Mr Krabs")
+def GenerateOptions(test,test2):
+    UserOptions = []
+
+    if(test == 1):
+        UserOptions.append("Look")
+
+    if(test2 == 2):
+        UserOptions.append("Go")
+
+    return UserOptions
+
+UserOptions = GenerateOptions(1,2)
+for x in UserOptions:
+    print(x)
