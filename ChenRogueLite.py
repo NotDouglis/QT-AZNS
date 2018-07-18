8e76e6b391de0fe7f70aecdb79a1a0179297d1b7
VallyMan <40832721+VallyMan@users.noreply.github.com>
2018-07-18 09:43:48 +0200

    Generate Array

--- ChenRogueLite.py
+++ ChenRogueLite.py
@@ -1,4 +1,29 @@
-def GenerateOptions(test,test2):
+import random
+
+#SituationArray will have 1 or 0 depending on the circumstances
+"""
+Array Position Values
+0 = Dark
+1 = Door North
+2 = Door South
+3 = Door East
+4 = Door West
+5 = Enemy
+"""
+
+def GenerateSituation():
+    SituationArray = []
+    for x in range(0,6):
+        SituationArray.append(random.randint(0,1))
+        print(x , " = " , SituationArray[x])     
+
+    return SituationArray
+
+SituationArray = GenerateSituation()
+
+
+"""
+def GenerateOptions(SituationArray):
     UserOptions = []
 
     if(test == 1):
@@ -9,6 +34,7 @@ def GenerateOptions(test,test2):
 
     return UserOptions
 
-UserOptions = GenerateOptions(1,2)
+UserOptions = GenerateOptions(1)
 for x in UserOptions:
     print(x)
+"""
