ce7912a98575e9bb0e619ffb42fd2644f7e96c6e
VallyMan <40832721+VallyMan@users.noreply.github.com>
2018-07-18 10:01:16 +0200

    Basic Situation and Option Generation

--- ChenRogueLite.py
+++ ChenRogueLite.py
@@ -11,30 +11,41 @@ Array Position Values
 5 = Enemy
 """
 
+#Generate Situation
 def GenerateSituation():
     SituationArray = []
     for x in range(0,6):
         SituationArray.append(random.randint(0,1))
         print(x , " = " , SituationArray[x])     
-
     return SituationArray
 
 SituationArray = GenerateSituation()
 
-
-"""
+#Generate Options based on the situation
 def GenerateOptions(SituationArray):
     UserOptions = []
 
-    if(test == 1):
-        UserOptions.append("Look")
+    if(SituationArray[0] == 1):
+        UserOptions.append("Light")
 
-    if(test2 == 2):
-        UserOptions.append("Go")
+    if(SituationArray[1] == 1):
+        UserOptions.append("Go North")
 
+    if(SituationArray[2] == 1):
+        UserOptions.append("Go South")
+
+    if(SituationArray[3] == 1):
+        UserOptions.append("Go East")
+
+    if(SituationArray[4] == 1):
+        UserOptions.append("Go West")
+
+    if(SituationArray[5] == 1):
+        UserOptions.append("Attack")
     return UserOptions
 
-UserOptions = GenerateOptions(1)
+UserOptions = GenerateOptions(SituationArray)
+
 for x in UserOptions:
     print(x)
-"""
+
